ae9e844a400bd91accf1e9db44399f1a868d71b9
Andreea Andro <andreeaandro19@gmail.com>
2018-05-10 00:03:01 +0300

    - small improvements on DatabaseHelperTest

--- facultyAdminssion/src/unitTests/databaseOperations/DatabaseHelperTest.java
+++ facultyAdminssion/src/unitTests/databaseOperations/DatabaseHelperTest.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import databaseOperations.DatabaseHelper;
@@ -20,11 +24,40 @@ import databaseOperations.DatabaseHelper;
  *	In lucru, a se vedea linkul https://docs.google.com/document/d/1CLcEc31n6SWkSICaTTf-Yx2cT1cuyyuEyIf39j6duKA/edit?usp=sharing
  *
  */
+@DisplayName("Test cases for DatabaseHelper implementation of DatabaseManager")
 class DatabaseHelperTest {
 
 	static String testTableName = "test_table_name12345.txt";
 	static String[] testColumns = new String[] {"test_col1", "test_col2"};
 	static String rowSeparator = "\r\n";
+	static DatabaseHelper dbh;
+	
+	@BeforeAll
+    static void initAll() {
+		dbh = new DatabaseHelper();
+    }
+	
+	@BeforeEach
+    void init() {
+		String columnNames="id-name-age-isStudent-";
+		String row1 = "1-Ana-19-yes-";
+		String row2 = "2-Mihai-20-yes-";
+		String row3 = "3-Doru-19-no-";
+		String tableContent = columnNames + rowSeparator 
+				+ row1 + rowSeparator 
+				+ row2 + rowSeparator 
+				+ row3 + rowSeparator;
+		
+		createFile(testTableName, tableContent);
+    }
+	
+	@AfterEach
+    void tearDown() {
+		File file = new File(testTableName);
+		if(file.exists()) {
+			file.delete();
+		}
+    }
 	
 	@Test
 	void testCreateTable_tableDidNotExistPreviously() {
@@ -35,7 +68,6 @@ class DatabaseHelperTest {
 		}
 		
 		//create the table
-		DatabaseHelper dbh = new DatabaseHelper();
 		dbh.createTable(testTableName, testColumns);
 
 		file = new File(testTableName);
@@ -46,85 +78,88 @@ class DatabaseHelperTest {
 		for(String column: testColumns) {
 			sb.append(column).append("-");
 		}
-		sb.append("\r\n");
+		sb.append(rowSeparator);
 		String expectedResult = sb.toString();
 		String actualResult = readFile(testTableName);
 		
-		System.out.println("**"+expectedResult+"***"+actualResult+"**");
-		assertEquals(expectedResult, actualResult);
-		
-		file.delete();
+		assertEquals(expectedResult, actualResult,  "createTable should make a new table and insert the column names");
 	}
 
 	@Test
+	@DisplayName("Nu adaugam de doua ori numele coloanelor")
 	void testCreateTable_tableAlreadyExists() {
-		//create a table 
-		String existingContent = "hard to guess";
-		try {
-			FileWriter fw = new FileWriter(testTableName, false);
-			fw.write(existingContent);
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//try to create again the same table, but do not change anything
-		DatabaseHelper dbh = new DatabaseHelper();
+		String existingContent = readFile(testTableName);
+		//try to create again the same table, but we know it already exists
 		dbh.createTable(testTableName, testColumns);
-
 		File file = new File(testTableName);
 		assertTrue(file.exists());
 
 		//verify the content to be as in the first table
 		String actualResult = readFile(testTableName);
-		
-		System.out.println("**"+existingContent+"***"+actualResult+"**");
-		assertEquals(existingContent, actualResult);
+		assertEquals(existingContent, actualResult,  "createTable should not alter an existing table");
 		
 		file.delete();
 	}
 	
 	@Test
-	void testInsertQuery_insertWhateverYouReceive() {
-		String existingContent = "hard to guess";
-		try {
-			FileWriter fw = new FileWriter(testTableName, false);
-			fw.write(existingContent);
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	void testInsertQuery_corectNumberOfColumns() {
+		String existingContent = readFile(testTableName);
 		
-		String[] toInsert = new String[] {"test_col1", "test_col2"};
-		DatabaseHelper dbh = new DatabaseHelper();
+		String[] toInsert = new String[] {"test_col1", "test_col2", "test_col3"};
 		dbh.insertQuery(testTableName, toInsert);
 		
 		String expectedContent = existingContent;
 		for(String cont: toInsert) {
 			expectedContent+= cont+"-";
 		}
-		expectedContent+="\r\n";
+		expectedContent+=rowSeparator;
 		
 		String actualContent = readFile(testTableName);
-		System.out.println("testInsertQuery_:\nActualContent: *" + actualContent+"*, expectedContent: *"+expectedContent+"* ");
 		
-		assertEquals(expectedContent, actualContent);
+		assertEquals(expectedContent, actualContent, "InsertQuery adds a new line with received values");
+	}
+	
+	@Test
+	void testInsertQuery_valuesLessThanColumns() {
+		String existingContent = readFile(testTableName);
+		
+		String[] toInsert = new String[] {"test_col3"};
+		dbh.insertQuery(testTableName, toInsert);
+		
+		String expectedContent = existingContent;
+		for(String cont: toInsert) {
+			expectedContent+= cont+"-";
+		}
+		expectedContent+=rowSeparator;
+		
+		String actualContent = readFile(testTableName);
+		
+		assertEquals(expectedContent, actualContent, "InsertQuery adds a new line when nr of received values is less then expected");
+	}
+	
+	@Test
+	void testInsertQuery_valuesMoreThanColumns() {
+		String existingContent = readFile(testTableName);
+		
+		String[] toInsert = new String[] {"test_col1", "test_col2", "test_col3", "test_col4", "test_col5"};
+		dbh.insertQuery(testTableName, toInsert);
+		
+		String expectedContent = existingContent;
+		for(String cont: toInsert) {
+			expectedContent+= cont+"-";
+		}
+		expectedContent+=rowSeparator;
+		
+		String actualContent = readFile(testTableName);
+		
+		assertEquals(expectedContent, actualContent, "InsertQuery adds a new line when nr of received values is less then expected");
 	}
 
 	@Test
 	void testDeleteQuery_whereAge19_2Rows() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-19-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
+		String columnNames="id-name-age-isStudent-";
+		String row2 = "2-Mihai-20-yes-";
 		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		dbh.deleteQuery(testTableName, "age", "19");
 		
 		String expectedResult = columnNames + rowSeparator + 
@@ -135,23 +170,15 @@ class DatabaseHelperTest {
 	}
 
 	@Test
-	void testDeleteQuery_whereAge18_1Row() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-18-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "18-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
+	void testDeleteQuery_whereAge20_1Row() {
+		String columnNames="id-name-age-isStudent-";
+		String row1 = "1-Ana-19-yes-";
+		String row3 = "3-Doru-19-no-";
 		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
-		dbh.deleteQuery(testTableName, "age", "18");
+		dbh.deleteQuery(testTableName, "age", "20");
 		
 		String expectedResult = columnNames + rowSeparator + 
-				row2 + rowSeparator + 
+				row1 + rowSeparator + 
 				row3 + rowSeparator;
 		String actualResult = readFile(testTableName);
 		
@@ -160,18 +187,17 @@ class DatabaseHelperTest {
 	
 	@Test
 	void testDeleteQuery_columnValueDoesNotExist() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
+		String columnNames="id-name-age-isStudent-";
+		String row1 = "1-Ana-19-yes-";
+		String row2 = "2-Mihai-20-yes-";
+		String row3 = "3-Doru-23-no-";
+		String tableContent = columnNames + rowSeparator 
+				+ row1 + rowSeparator 
+				+ row2 + rowSeparator 
+				+ row3 + rowSeparator;
 		
 		createFile(testTableName, tableContent);
 		
-		DatabaseHelper dbh = new DatabaseHelper();
 		dbh.deleteQuery(testTableName, "name", "Oana");
 		
 		String actualResult = readFile(testTableName);
@@ -181,58 +207,34 @@ class DatabaseHelperTest {
 	
 	@Test
 	void testDeleteQuery_columnNameDoesNotExist() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
+		String tableContent = readFile(testTableName);
 		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		dbh.deleteQuery(testTableName, "knowsToDance", "Oana");
 		
 		String actualResult = readFile(testTableName);
 		
-		System.out.println("tableContent -*"+tableContent+"*-");
-		System.out.println("actualResult -*"+actualResult+"*-");
 		assertEquals(tableContent, actualResult);
 	}
 	
 	@Test
 	void testSelectQuery_normalTable() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
-		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		List<List<String>> actualResult = dbh.selectQuery(testTableName);
 		
 		List<List<String>> expectedResult = Arrays.asList(
 				Arrays.asList("1", "Ana", "19", "yes"),
 				Arrays.asList("2", "Mihai", "20", "yes"),
-				Arrays.asList("3", "Doru", "23", "no")
+				Arrays.asList("3", "Doru", "19", "no")
 				);
 		
-		assertEquals(actualResult, expectedResult);
+		assertEquals(actualResult, expectedResult, "SelectQuery should return all the values from the table");
 	}
-
+	
 	@Test
 	void testSelectQuery_differentLengthRows() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes-Ana";
-		String row2 = "2-Mihai--yes";
-		String row3 = "3-null-23";
+		String columnNames="id-name-age-isStudent-";
+		String row1 = "1-Ana-19-yes-Ana-";
+		String row2 = "2-Mihai--yes-";
+		String row3 = "3-null-23-";
 		String tableContent = columnNames + rowSeparator + 
 				row1 + rowSeparator + 
 				row2 + rowSeparator + 
@@ -240,7 +242,6 @@ class DatabaseHelperTest {
 		
 		createFile(testTableName, tableContent);
 		
-		DatabaseHelper dbh = new DatabaseHelper();
 		List<List<String>> actualResult = dbh.selectQuery(testTableName);
 		
 		List<List<String>> expectedResult = Arrays.asList(
@@ -249,23 +250,11 @@ class DatabaseHelperTest {
 				Arrays.asList("3", "null", "23")
 				);
 		
-		assertEquals(actualResult, expectedResult);
+		assertEquals(actualResult, expectedResult, "SelectQuery should return all the values from the table");
 	}
 	
 	@Test
 	void testSelectWhereQuery_forIsStudentYes_1listContaining1rows() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
-		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		String[] columnName = {"id", "name", "isStudent"};
 		ArrayList<String> actualResult = dbh.selectWhereQuery(testTableName, columnName, "id", "3");
 		
@@ -276,18 +265,6 @@ class DatabaseHelperTest {
 	
 	@Test
 	void testSelectWhereQuery_forIsStudentYes_1listContaining2rows() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
-		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		String[] columnName = {"id", "name", "isStudent"};
 		ArrayList<String> actualResult = dbh.selectWhereQuery(testTableName, columnName, "isStudent", "yes");
 		
@@ -298,18 +275,6 @@ class DatabaseHelperTest {
 	
 	@Test
 	void testSelectWhereQuery_forIsStudentWhat_emptyList() {
-		String columnNames="id-name-age-isStudent";
-		String row1 = "1-Ana-19-yes";
-		String row2 = "2-Mihai-20-yes";
-		String row3 = "3-Doru-23-no";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
-		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
 		String[] columnName = {"id", "name", "isStudent"};
 		ArrayList<String> actualResult = dbh.selectWhereQuery(testTableName, columnName, "isStudent", "what?");
 		
@@ -321,18 +286,7 @@ class DatabaseHelperTest {
 
 	@Test
 	void testUpdateQuery_setAge21whereAge20_1column() {
-		String columnNames="id-name-age-isStudent-";
-		String row1 = "1-Ana-19-yes-";
-		String row2 = "2-Mihai-20-yes-";
-		String row3 = "3-Doru-23-no-";
-		String tableContent = columnNames + rowSeparator + 
-				row1 + rowSeparator + 
-				row2 + rowSeparator + 
-				row3 + rowSeparator;
-		
-		createFile(testTableName, tableContent);
-		
-		DatabaseHelper dbh = new DatabaseHelper();
+		String tableContent = readFile(testTableName);
 		String[] newColumnValue = {"2", "Mihai", "21", "yes"};
 		dbh.updateQuery(testTableName, "age", "20", newColumnValue);
 		
